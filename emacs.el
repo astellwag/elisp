95c4513335bb6ab18fb5631ed765c1d216447138
Alex Stellwag <alex@aragorn.thorwinsworld.de>
2017-02-13 12:44:45 +0100

    first step towards modularization

--- emacs.el
+++ emacs.el
@@ -6,7 +6,14 @@
 ;; -------------------------------------------------------------------
 
 ;; elisp base-path
-(defvar ast-elisp-dir "~/elisp/")
+(defvar ast-elisp-dir "~/github/elisp/")
+
+;; Datei laden, wenn vorhanden, sonst Warnung
+(defun ast-load-part (part)
+  (let ((fullname (concat ast-elisp-dir part)))
+    (if (file-exists-p fullname)
+	(load fullname)
+      (message (format "Loading %s (source)...failed" fullname)))))
 
 ;; path to local packages
 (add-to-list 'load-path (expand-file-name (concat ast-elisp-dir "packages")))
--- gnus.el
+++ gnus.el
@@ -1,3 +1,21 @@
+;; -*- emacs-lisp -*-
+;; -------------------------------------------------------------------
+;;
+;; gnus.el - My gnus-configuration
+;;
+;; -------------------------------------------------------------------
+
+;; elisp base-path
+(defvar ast-elisp-dir "~/github/elisp/")
+
+;; Datei laden, wenn vorhanden, sonst Warnung
+(defun ast-load-part (part)
+  (let ((fullname (concat ast-elisp-dir part)))
+    (if (file-exists-p fullname)
+	(load fullname)
+      (message (format "Loading %s (source)...failed" fullname)))))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; me, myself and I
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
