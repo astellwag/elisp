6ab56775729717e19488ed654e9074e5555e49d1
Alex Stellwag <alex@stellwag.net>
2018-10-28 11:10:49 +0100

    let's start over freshly

--- gnus-message.el
+++
@@ -1,9 +0,0 @@
-;; -*- emacs-lisp -*-
-;; -------------------------------------------------------------------
-;;
-;; gnus-message.el - message buffer settings
-;;
-;; -------------------------------------------------------------------
-
-;; show _some_ headers
-(setq gnus-visible-headers "^From:\\|^To:\\|^Subject:\\|^Date:\\|^User-Agent:")
